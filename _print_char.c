321abda157b4d5757dcc2842593d7dcf5b4bb003
davidson961 <125483143+davidson961@users.noreply.github.com>
2023-06-26 12:14:02 +0100

    Update _print_char.c

--- _print_char.c
+++ _print_char.c
@@ -9,26 +9,3 @@ int print_char(va_list list)
 _write_char(va_arg(list, int));
 return (1);
 }
-
-_print_string.c
-
-
-#include "main.h"
-
-/**
- * print_string - Prints a string
- * @list: list of arguments
- * Return: Will return the amount of characters printed.
- */
-int print_string(va_list list)
-{
-int i;
-char *str;
-
-str = va_arg(list, char *);
-if (str == NULL)
-str = "(null)";
-for (i = 0; str[i] != '\0'; i++)
-_write_char(str[i]);
-return (i);
-}
