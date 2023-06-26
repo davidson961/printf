aa1673073c7449264d04c16df4f281687fdb5598
davidson961 <125483143+davidson961@users.noreply.github.com>
2023-06-26 18:49:48 +0100

    _printf.c

--- _printf.c
+++ _printf.c
@@ -1,66 +1,96 @@
 #include "main.h"
 
-void print_buffer(char buffer[], int *buff_ind);
+#define BUFF_SIZE 1024
 
-/**
-   * _printf - Printf function
-    * @format: format.
-     * Return: Printed chars.
-      */
+void print_buffer(char buffer[], int *buff_ind);
+int get_flags(const char *format, int *i);
+int get_width(const char *format, int *i, va_list list);
+int get_precision(const char *format, int *i, va_list list);
+int get_size(const char *format, int *i);
+int handle_print(const char *format, int *i, va_list list, char buffer[],
+                 int flags, int width, int precision, int size);
+
 int _printf(const char *format, ...)
 {
-	int i, printed = 0, printed_chars = 0;
-	int flags, width, precision, size, buff_ind = 0;
-	va_list list;
-	char buffer[BUFF_SIZE];
+    int i, printed = 0, printed_chars = 0;
+    int flags, width, precision, size, buff_ind = 0;
+    va_list list;
+    char buffer[BUFF_SIZE];
 
-	if (format == NULL)
-		return (-1);
+    if (format == NULL)
+        return -1;
 
-	va_start(list, format);
+    va_start(list, format);
 
-	for (i = 0; format && format[i] != '\0'; i++)
-	{
-		if (format[i] != '%')
-		{
-			buffer[buff_ind++] = format[i];
-			if (buff_ind == BUFF_SIZE)
-				print_buffer(buffer, &buff_ind);
+    for (i = 0; format[i] != '\0'; i++)
+    {
+        if (format[i] != '%')
+        {
+            buffer[buff_ind++] = format[i];
+            if (buff_ind == BUFF_SIZE)
+                print_buffer(buffer, &buff_ind);
 
-			printed_chars++;
-		}
-		else
-		{
-			print_buffer(buffer, &buff_ind);
-			flags = get_flags(format, &i);
-			width = get_width(format, &i, list);
-			precision = get_precision(format, &i, list);
-			size = get_size(format, &i);
-			++i;
-			printed = handle_print(format, &i, list, buffer,
-				flags, width, precision, size);
-			if (printed == -1)
-				return (-1);
-			printed_chars += printed;
-		}
-	}
+            printed_chars++;
+        }
+        else
+        {
+            print_buffer(buffer, &buff_ind);
+            flags = get_flags(format, &i);
+            width = get_width(format, &i, list);
+            precision = get_precision(format, &i, list);
+            size = get_size(format, &i);
+            ++i;
+            printed = handle_print(format, &i, list, buffer,
+                                   flags, width, precision, size);
+            if (printed == -1)
+                return -1;
+            printed_chars += printed;
+        }
+    }
 
-	print_buffer(buffer, &buff_ind);
+    print_buffer(buffer, &buff_ind);
 
-	va_end(list);
+    va_end(list);
 
-	return (printed_chars);
+    return printed_chars;
 }
 
-/**
-   * print_buffer - Prints the contents of the buffer if it exist
-    * @buffer: Array of chars
-     * @buff_ind: Index at which to add next char, represents the length.
-      */
 void print_buffer(char buffer[], int *buff_ind)
 {
-	if (*buff_ind > 0)
-		write(1, &buffer[0], *buff_ind);
+    if (*buff_ind > 0)
+    {
+        write(1, buffer, *buff_ind);
+        *buff_ind = 0;
+    }
+}
 
-	*buff_ind = 0;
+int get_flags(const char *format, int *i)
+{
+    // Placeholder for extracting and returning the flags from the format string
+    return 0;
+}
+
+int get_width(const char *format, int *i, va_list list)
+{
+    // Placeholder for extracting and returning the width from the format string
+    return 0;
+}
+
+int get_precision(const char *format, int *i, va_list list)
+{
+    // Placeholder for extracting and returning the precision from the format string
+    return 0;
+}
+
+int get_size(const char *format, int *i)
+{
+    // Placeholder for extracting and returning the size from the format string
+    return 0;
+}
+
+int handle_print(const char *format, int *i, va_list list, char buffer[],
+                 int flags, int width, int precision, int size)
+{
+    // Placeholder for handling the printing based on the conversion specifier and other parameters
+    return 0;
 }
