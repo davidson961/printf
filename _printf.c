538909f505b672b709e3e67b16c305e82a7bb6dd
chepkemoi796 <chepkemoi796@gmail.com>
2023-06-26 13:38:38 -0700

    _printf function

--- _printf.c
+++ _printf.c
@@ -1,96 +1,81 @@
 #include "main.h"
-
 #define BUFF_SIZE 1024
-
 void print_buffer(char buffer[], int *buff_ind);
 int get_flags(const char *format, int *i);
 int get_width(const char *format, int *i, va_list list);
 int get_precision(const char *format, int *i, va_list list);
 int get_size(const char *format, int *i);
 int handle_print(const char *format, int *i, va_list list, char buffer[],
-                 int flags, int width, int precision, int size);
+int flags, int width, int precision, int size);
 
 int _printf(const char *format, ...)
 {
-    int i, printed = 0, printed_chars = 0;
-    int flags, width, precision, size, buff_ind = 0;
-    va_list list;
-    char buffer[BUFF_SIZE];
-
-    if (format == NULL)
-        return -1;
-
-    va_start(list, format);
-
-    for (i = 0; format[i] != '\0'; i++)
-    {
-        if (format[i] != '%')
-        {
-            buffer[buff_ind++] = format[i];
-            if (buff_ind == BUFF_SIZE)
-                print_buffer(buffer, &buff_ind);
-
-            printed_chars++;
-        }
-        else
-        {
-            print_buffer(buffer, &buff_ind);
-            flags = get_flags(format, &i);
-            width = get_width(format, &i, list);
-            precision = get_precision(format, &i, list);
-            size = get_size(format, &i);
-            ++i;
-            printed = handle_print(format, &i, list, buffer,
-                                   flags, width, precision, size);
-            if (printed == -1)
-                return -1;
-            printed_chars += printed;
-        }
-    }
-
-    print_buffer(buffer, &buff_ind);
-
-    va_end(list);
-
-    return printed_chars;
+int i, printed = 0, printed_chars = 0;
+int flags, width, precision, size, buff_ind = 0;
+va_list list;
+char buffer[BUFF_SIZE];
+if (format == NULL)
+return -1;
+va_start(list, format);
+for (i = 0; format[i] != '\0'; i++)
+{
+if (format[i] != '%')
+{
+buffer[buff_ind++] = format[i];
+if (buff_ind == BUFF_SIZE)
+print_buffer(buffer, &buff_ind);
+printed_chars++;
 }
-
+else
+{
+print_buffer(buffer, &buff_ind);
+flags = get_flags(format, &i);
+width = get_width(format, &i, list);
+precision = get_precision(format, &i, list);
+size = get_size(format, &i);
+++i;
+printed = handle_print(format, &i, list, buffer,
+flags, width, precision, size);
+if (printed == -1)
+return -1;
+printed_chars += printed;
+}
+}
+print_buffer(buffer, &buff_ind);
+va_end(list);
+return printed_chars;
+}
 void print_buffer(char buffer[], int *buff_ind)
 {
-    if (*buff_ind > 0)
-    {
-        write(1, buffer, *buff_ind);
-        *buff_ind = 0;
-    }
+if (*buff_ind > 0)
+{
+write(1, buffer, *buff_ind);
+*buff_ind = 0;
 }
-
+}
 int get_flags(const char *format, int *i)
 {
-    // Placeholder for extracting and returning the flags from the format string
-    return 0;
+/* Placeholder for extracting and returning the flags from the format string */
+return 0;
 }
-
 int get_width(const char *format, int *i, va_list list)
 {
-    // Placeholder for extracting and returning the width from the format string
-    return 0;
+/* Placeholder for extracting and returning the width from the format string */
+return 0;
 }
-
 int get_precision(const char *format, int *i, va_list list)
 {
-    // Placeholder for extracting and returning the precision from the format string
-    return 0;
+/* Placeholder for extracting and returning the precision from the format string */
+return 0;
 }
-
 int get_size(const char *format, int *i)
 {
-    // Placeholder for extracting and returning the size from the format string
-    return 0;
+/* Placeholder for extracting and returning the size from the format string */
+return 0;
 }
-
 int handle_print(const char *format, int *i, va_list list, char buffer[],
-                 int flags, int width, int precision, int size)
+ int flags, int width, int precision, int size)
 {
-    // Placeholder for handling the printing based on the conversion specifier and other parameters
-    return 0;
+/* Placeholder for handling the printing based on the conversion specifier and other parameters */
+return 0;
 }
