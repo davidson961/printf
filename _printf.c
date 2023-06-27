137816debec01c6f9551a7cdfed02e25deb3a3d1
davidson961 <125483143+davidson961@users.noreply.github.com>
2023-06-27 12:16:15 +0100

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,104 +1,109 @@
-#include "main.h"
+#include <stdarg.h>
 #include <unistd.h>
+
 #define BUFF_SIZE 1024
+
 void print_buffer(char buffer[], int *buff_ind);
+int get_flags(const char *format, int *i);
+int get_width(const char *format, int *i, va_list list);
+int get_precision(const char *format, int *i, va_list list);
+int get_size(const char *format, int *i);
 int handle_print(const char *format, int *i, va_list list, char buffer[],
-int flags, int width, int precision, int size);
-int handle_write_char(char c, char buffer[], int flags, int width, int precision, int size);
+                 int flags, int width, int precision, int size);
 
 int _printf(const char *format, ...)
 {
-int i, printed = 0, printed_chars = 0;
-int flags, width, precision, size, buff_ind = 0;
-va_list list;
-char buffer[BUFF_SIZE];
-if (format == NULL)
-return -1;
-va_start(list, format);
-for (i = 0; format[i] != '\0'; i++)
-{
-if (format[i] != '%')
-{
-buffer[buff_ind++] = format[i];
-if (buff_ind == BUFF_SIZE)
-print_buffer(buffer, &buff_ind);
-printed_chars++;
+    int i, printed = 0, printed_chars = 0;
+    int flags, width, precision, size, buff_ind = 0;
+    va_list list;
+    char buffer[BUFF_SIZE];
+
+    if (format == NULL)
+        return -1;
+
+    va_start(list, format);
+
+    for (i = 0; format[i] != '\0'; i++)
+    {
+        if (format[i] != '%')
+        {
+            buffer[buff_ind++] = format[i];
+            if (buff_ind == BUFF_SIZE)
+                print_buffer(buffer, &buff_ind);
+
+            printed_chars++;
+        }
+        else
+        {
+            print_buffer(buffer, &buff_ind);
+            flags = get_flags(format, &i);
+            width = get_width(format, &i, list);
+            precision = get_precision(format, &i, list);
+            size = get_size(format, &i);
+            ++i;
+            printed = handle_print(format, &i, list, buffer,
+                                   flags, width, precision, size);
+            if (printed == -1)
+                return -1;
+            printed_chars += printed;
+        }
+    }
+
+    print_buffer(buffer, &buff_ind);
+
+    va_end(list);
+
+    return printed_chars;
 }
-else
-{
-print_buffer(buffer, &buff_ind);
-flags = 0; /* call get_flags(format, &i) */
-width = 0; /* get_width(format, &i, list) */
-precision = 0; /* get_precision(format, &i, list) */
-size = 0; /* get_size(format, &i) */
-++i;
-printed = handle_print(format, &i, list, buffer,
-flags, width, precision, size);
-if (printed == -1)
-return -1;
-printed_chars += printed;
-}
-}
-print_buffer(buffer, &buff_ind);
-va_end(list);
-return printed_chars;
-}
+
 void print_buffer(char buffer[], int *buff_ind)
 {
-if (*buff_ind > 0)
-{
-write(1, buffer, *buff_ind);
-*buff_ind = 0;
+    if (*buff_ind > 0)
+    {
+        write(1, buffer, *buff_ind);
+        *buff_ind = 0;
+    }
 }
-}
-int handle_write_char(char c, char buffer[], int flags, int width, int precision, int size)
+
+int get_flags(const char *format, int *i)
 {
-int i = 0;
-char padd = '';
-/* unused parameter */
-(void)size;
-(void)precisio;
-if (flags & F_ZERO)
-padd = '0';
-buffer[i++] = c;
-buffer[i] = '\0';
-if (width > 1)
-{
-buffer[BUFF_SIZE - 1] = '\0';
-for (i = 0; i < width - 1; i++)
-buffer[BUFF_SIZE - i - 2] = padd;
-if (flags & F_MINUS)
-return write(1, &buffer[0], 1) + write(1, &buffer[BUFF_SIZE - i - 1], width - 1);
-else
-return write(1, &buffer[BUFF_SIZE - i - 1], width - 1) + write(1, &buffer[0], 1);
+    // Placeholder for the implementation of the 'get_flags' function
+    // It should extract and return the flags from the format string at position *i
+    // Modify the function as per your requirements
+    return 0;
 }
-return write(1, &buffer[0], 1);
-}
-int handle_print(const char *format, int *i, va_list, char buffer[], int flags, int width, int precision, int size)
+
+int get_width(const char *format, int *i, va_list list)
 {
-int unknow_len = 0, printed_chars = -1;
-fmt_t fmt_types[] = {
-{'c', handle_write_char},
-/* add other format specifiers and theri correspondig handling functions here */
-{'\0', NULL}
-};
-for (int j = 0; fmt_types[j].fmt != '\0'; j++)
-{
-if (format[*i] == fmt_types[j].fmt)
-{
-if (fmt_types[j].func != NULL)
-printed_chars = fmt_types[j].func(va_arg(list, int), buffer, flags, width, precision, size);
-unknow_len = 1;
-break;
+    // Placeholder for the implementation of the 'get_width' function
+    // It should extract and return the width from the format string at position *i
+    // You can use the 'va_arg' macro to get the next argument from the variable argument list
+    // Modify the function as per your requirements
+    return 0;
 }
-}
-if (!unknow_len)
+
+int get_precision(const char *format, int *i, va_list list)
 {
-buffer[0] = '%';
-buffer[1] = format[*i];
-buffer[2] = '\0';
-printed_chars = handle_write_char('%', buffer, flags, width, precision, size);
-printed_chars += handle_write_char(format[*i], buffer, flags, width, precision,size);
+    // Placeholder for the implementation of the 'get_precision' function
+    // It should extract and return the precision from the format string at position *i
+    // You can use the 'va_arg' macro to get the next argument from the variable argument list
+    // Modify the function as per your requirements
+    return 0;
 }
-return printed_chars;
+
+int get_size(const char *format, int *i)
+{
+    // Placeholder for the implementation of the 'get_size' function
+    // It should extract and return the size from the format string at position *i
+    // Modify the function as per your requirements
+    return 0;
+}
+
+int handle_print(const char *format, int *i, va_list list, char buffer[],
+                 int flags, int width, int precision, int size)
+{
+    // Placeholder for the implementation of the 'handle_print' function
+    // It should handle the printing based on the conversion specifier and other parameters
+    // Modify the function as per your requirements
+    return 0;
 }
